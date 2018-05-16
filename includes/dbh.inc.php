185babd74634efa02096740578cb9326208b0d53
semirzulic <semirzulic92@gmail.com>
2018-05-16 09:56:58 +0200

    Modified the connection link and tables

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,54 +1,52 @@
+
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbName = "";
+$dbhost  = 'localhost';
+$dbuser  = 'root';
+$dbpass  = '';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if (!$conn) {
+    die('SERVER CONNECTION FAILED...\n: ' . mysqli_error());
+}
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbName);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+$sql    = 'CREATE DATABASE IF NOT EXISTS myDB';
+$retval = mysqli_query( $conn, $sql);
+if (!$retval) {
+    die('DATABASE CREATION FAILED\n: ' . mysqli_error());
+}
 
-// Create database
-$sql = "CREATE DATABASE IF NOT EXISTS myDB";
-if ($conn->query($sql) === FALSE) {
-    echo "Error creating database: " . $conn->error;
-} 
+mysqli_select_db($conn,'myDB');
 
+//Crete Tabele
+$sql = "CREATE TABLE IF NOT EXISTS libusers (
+        id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+        firstname VARCHAR(256) NOT NULL,
+        lastname VARCHAR(256) NOT NULL,
+        email VARCHAR(256) NOT NULL,
+        password VARCHAR(256) NOT NULL,
+        role VARCHAR(256) NOT NULL
+        )";
 
-// Create Table
-
-$mysql = "CREATE TABLE IF NOT EXISTS myDB.users (
-id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(256) NOT NULL,
-lastname VARCHAR(256) NOT NULL,
-email VARCHAR(256) NOT NULL,
-password VARCHAR(256) NOT NULL,
-role VARCHAR(256) NOT NULL
-)";
-
-if ($conn->query($mysql) === FALSE) {
-  	echo "Error creating table: " . $conn->error;
-	}
+$retval = mysqli_query( $conn, $sql);
+if (!$retval) {
+    die('COULD NOT CREATE TABLE\n: ' . mysqli_error());
+}
 
 
 // Create another Table
-$mysql = "CREATE TABLE IF NOT EXISTS myDB.books (
+$sql = "CREATE TABLE IF NOT EXISTS books (
 id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 title VARCHAR(256) NOT NULL,
 author VARCHAR(256) NOT NULL,
 genre VARCHAR(256) NOT NULL
 )";
 
-if ($conn->query($mysql) === FALSE) {
-  	echo "Error creating table: " . $conn->error;
-	}
+$retval = mysqli_query( $conn, $sql);
+if (!$retval) {
+    die('COULD NOT CREATE TABLE\n: ' . mysqli_error());
+}
 
-	
 
-$conn->close();
+
 ?>
 
 
